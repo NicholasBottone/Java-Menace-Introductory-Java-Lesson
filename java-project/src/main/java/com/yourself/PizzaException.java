ef92a129be3feb7b03b2c889e710d530d1742467
Nicholas Bottone <NicholasBottone@gmail.com>
2018-10-22 19:02:32 -0400

    Update java-project/src/main/java/com/yourself/PizzaException.java

--- java-project/src/main/java/com/yourself/PizzaException.java
+++ java-project/src/main/java/com/yourself/PizzaException.java
@@ -2,4 +2,8 @@ package com.yourself;
 
 public class PizzaException extends Exception {
     private static final long serialVersionUID = 123;
+
+    public PizzaException(String message) {
+        super(message);
+    }
 }
